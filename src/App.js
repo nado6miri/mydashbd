b7390d3beee530fa7072a1a4d7580c00bbeb7306
nado6miri <nado6miri@gmail.com>
2023-03-27 21:49:30 +0900

    Lifecycle 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+
 //import Layout from "./components/Layout.js";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -8,8 +10,20 @@ import State_Ex from "./study/state/state_Ex";
 import EventHandling_Ex from "./study/eventhandling/eventhandling_Ex";
 import Ref_Ex from "./study/ref/ref_Ex";
 import Iteration_Ex from "./study/iteration_map_key/map_Ex";
+import LifeCycleSample from "./study/lifecycle/lifecycle_Ex";
+import ErrorBoundary from "./study/lifecycle/errorCatch";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 function App() {
+  const [color, setColor] = useState("#000000");
+
+  const handleClick = () => {
+    setColor(getRandomColor());
+  };
+
   return (
     <div>
       <Header />
@@ -19,6 +33,11 @@ function App() {
       <EventHandling_Ex />
       <Ref_Ex />
       <Iteration_Ex />
+      <br></br>
+      <button onClick={handleClick}>랜덤색상</button>
+      <ErrorBoundary>
+        <LifeCycleSample color={color} />
+      </ErrorBoundary>
     </div>
   );
 }
